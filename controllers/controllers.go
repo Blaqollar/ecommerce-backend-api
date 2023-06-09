01907be8e63a922c11283e281cfd9ac4222beb62
Dingba Nice <uchedingba@gmail.com>
2023-06-09 02:37:37 +0100

    Controllers: adding the signup function

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,6 +1,17 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Blaqollar/ecommerce-backend-api/models"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/net/context"
+)
 
 func HashPassword(password string) string {
 
@@ -11,7 +22,69 @@ func VerifyPassword(UserPassword, GivenPassword string) (bool, string) {
 }
 
 func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var user models.User
+		err := c.BindJSON(&user)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validate := validator.New()
+		err = validate.Struct(user)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "user already exists"})
+		}
+
+		count, err = UserCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "phone number is already in use"})
+			return
+		}
+
+		user.ID = primitive.NewObjectID().Hex()
+		password := HashPassword(user.Password)
+		user.UserId = user.ID
+		user.Password = password
+		user.CreatedAt = time.Now().UTC()
+		user.UpdatedAt = time.Now().UTC()
+
+		token, refreshtoken, _ := generate.TokenGenerator(user.Email, user.FirstName, user.LastName, user.UserId)
+		user.Token = token
+		user.RefreshToken = refreshtoken
+		user.UserCart = make([]models.ProductUser, 0)
+		user.AddressDetails = make([]models.Address, 0)
+		user.OrderStatus = make([]models.Order, 0)
+
+		_, err = UserCollection.InsertOne(ctx, user)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "user was not created"})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusCreated, "successfully created")
+	}
 }
 
 func Login() gin.HandlerFunc {
