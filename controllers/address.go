4e1e71f3f7016f112d7777d729c6c0a12d880490
Dingba Nice <uchedingba@gmail.com>
2023-06-15 16:10:31 +0100

    Address: updated delete address function

--- controllers/address.go
+++ controllers/address.go
@@ -1,6 +1,15 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"time"
+
+	"github.com/Blaqollar/ecommerce-backend-api/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/net/context"
+)
 
 func AddAddress() gin.HandlerFunc {
 
@@ -15,5 +24,35 @@ func EditWorkAddress() gin.HandlerFunc {
 }
 
 func DeleteAddress() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		user_Id := c.Query("_id")
 
+		if user_Id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusInternalServerError, gin.H{"Error": "UserId is empty"})
+			c.Abort()
+		}
+
+		address := make([]models.Address, 0)
+
+		userID, err := primitive.ObjectIDFromHex(user_Id)
+		if err != nil {
+			c.JSON(404, "Internal error")
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		filter := bson.M{"_id": userID}
+		update := bson.M{"$set": bson.M{"address": address}}
+
+		_, err = UserCollection.UpdateOne(ctx, filter, update)
+		if err != nil {
+			c.JSON(404, "address not updated")
+		}
+		defer cancel()
+		ctx.Done()
+
+		c.JSON(200, "successfully deleted address")
+	}
 }
