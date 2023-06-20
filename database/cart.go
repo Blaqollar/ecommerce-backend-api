b99f829822dba1ca8ae15e1ecaf6b9f779fc8d8d
Dingba Nice <uchedingba@gmail.com>
2023-06-21 00:29:42 +0100

    database: adding AddToCart function

--- database/cart.go
+++ database/cart.go
@@ -1,10 +1,45 @@
 package database
 
+import (
+	"context"
+	"errors"
+	"log"
 
+	"github.com/Blaqollar/ecommerce-backend-api/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
+func AddToCart(ctx context.Context, prodCollection, userCollection *mongo.Collection, productId, userID string) error {
+	product, err := prodCollection.Find(ctx, bson.M{"_id": productId})
+	if err != nil {
+		log.Println(err)
+		return errors.New("could not find product")
+	}
 
-func AddToCart() {
+	var productCart []models.ProductUser
 
+	err = product.All(ctx, &productCart)
+	if err != nil {
+		log.Println(err)
+		return errors.New("could not decode product")
+	}
+
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		log.Println(err)
+		return errors.New("user ID not valid")
+	}
+
+	filter := bson.M{"_id": id}
+	update := bson.D{{Key: "$push", Value: bson.D{primitive.E{Key: "usercart", Value: bson.D{{Key: "$each", Value: productCart}}}}}}
+
+	_, err = userCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return errors.New("can't update user")
+	}
+	return nil
 }
 
 func RemoveItem() {
