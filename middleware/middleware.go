56845d70be6d77ebe69c4d92569080b54529ce9a
Dingba Nice <uchedingba@gmail.com>
2023-06-21 00:00:00 +0100

    Middleware: adding authentication function

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,1 +1,28 @@
 package middleware
+
+import (
+	"net/http"
+
+	"github.com/Blaqollar/ecommerce-backend-api/tokens"
+	"github.com/gin-gonic/gin"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "No authorization"})
+			c.Abort()
+		}
+
+		claims, err := tokens.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("uid", claims.Uid)
+		c.Next()
+	}
+}
